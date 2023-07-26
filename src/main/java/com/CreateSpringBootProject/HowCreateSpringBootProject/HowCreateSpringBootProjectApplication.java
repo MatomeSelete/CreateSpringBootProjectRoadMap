e19a603ee690e7bec49d87f329b507d7d0089320
Matome <seletematome3@gmail.com>
2023-07-26 15:14:45 +0200

    first commit

---
+++ src/main/java/com/CreateSpringBootProject/HowCreateSpringBootProject/HowCreateSpringBootProjectApplication.java
@@ -0,0 +1,13 @@
+package com.CreateSpringBootProject.HowCreateSpringBootProject;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HowCreateSpringBootProjectApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(HowCreateSpringBootProjectApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/CreateSpringBootProject/HowCreateSpringBootProject/HowCreateSpringBootProjectApplicationTests.java
@@ -0,0 +1,13 @@
+package com.CreateSpringBootProject.HowCreateSpringBootProject;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HowCreateSpringBootProjectApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
